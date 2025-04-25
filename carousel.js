ffddaeb2a6bae6140bfbbe16603294708d721d6f
liaber <79521946+liaber@users.noreply.github.com>
2025-04-25 00:01:45 -0400

    Carousel Image Scaling

--- carousel.js
+++ carousel.js
@@ -2,20 +2,20 @@ document.getElementById("left").onclick = previous;
 document.getElementById("right").onclick = next;
 
 let projectTitles = ["PyUI", "LED Matrix", "Robotic Arm", "Wifi-enabled Camera"];
-let projectImages = ["Assets/Python-Logo.png","Assets/download.jpg","Assets/images.jpg","Assets/canon_eosr8.jpg"];
+let projectImages = ["Assets/Python-Logo.jpg","Assets/download.jpg","Assets/images.jpg","Assets/canon_eosr8.jpg"];
 
 let currentProject = 0;
 let carousel = document.getElementById("project-carousel");
 let text = document.getElementById("project-carousel").children[0]
+
+setProject();
 
 function previous() {
     currentProject--;
     if (currentProject < 0) {
         currentProject = projectTitles.length - 1;
     }
-    console.log(currentProject);
-    carousel.style.backgroundImage = `url(${projectImages[currentProject]})`;
-    text.innerHTML = projectTitles[currentProject];
+    setProject()
 }
 
 function next() {
@@ -23,7 +23,14 @@ function next() {
     if (currentProject == projectTitles.length) {
         currentProject = 0;
     }
+    setProject()
+}
+
+function setProject() {
     console.log(currentProject);
     carousel.style.backgroundImage = `url(${projectImages[currentProject]})`;
+    carousel.style.backgroundSize = "cover";
+    carousel.style.backgroundPosition = "center";
+    carousel.style.backgroundRepeat = "no-repeat";
     text.innerHTML = projectTitles[currentProject];
 }
